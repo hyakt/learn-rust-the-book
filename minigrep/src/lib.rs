369fda0136d11b9c8247c4d3de98d1a332bf69d5
hyakt <hyakt0@gmail.com>
2021-01-27 01:36:23 +0900

    13

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,6 @@
-use std::{env, error::Error};
 use std::fs::File;
 use std::io::prelude::*;
+use std::{env, error::Error};
 
 pub struct Config {
     pub query: String,
@@ -9,17 +9,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enogh arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename, case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -43,24 +52,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
 }
 
 #[cfg(test)]
@@ -71,7 +67,7 @@ mod test {
     fn case_sensitive() {
         let query = "duct";
         let contents = "\
-Rust:
+        Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
@@ -84,11 +80,14 @@ Duct tape.";
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
-Rust:
+        Rust:
 safe, fast, productive.
 Pick three.
 Trust me";
 
-        assert_eq!(vec!["Rust:", "Trust me"], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
